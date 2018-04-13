10c78eb47148e70888a7bab2278d5d943ae0d5bf
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-04-13 12:46:49 -0400

    shell basics?

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -89,6 +89,21 @@ int main(void)
             break;
         }
 
+        pid_t pid = fork();
+        if (pid == -1)
+        {
+          printf("Fork error\n");
+        }
+        else if (pid > 0)
+        {
+          int status;
+          waitpid(pid, &status, 0);
+        }
+        else
+        {
+          execvp(args[0], &args[0]);
+        }
+
         #if DEBUG
 
         // Some debugging output
